b07fd3950943c3c67bc981d5a5a06e0295936df6
Chun Chen <chenchun.feed@gmail.com>
2014-12-22 23:39:07 +0800

    Minimum Path Sum

---
+++ accumulation-leetcode/src/main/java/org/chenchun/MinimumPathSum.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.chenchun;
+
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int r = grid.length, c = grid[0].length;
+        int dp[][] = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        dp[i][j] = grid[i][j];
+                    } else {
+                        dp[i][j] = dp[i][j-1] + grid[i][j];
+                    }
+                } else if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[r-1][c-1];
+    }
+}
