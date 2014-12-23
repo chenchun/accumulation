a851fb1cebfcd5c096f331b3abbe7257af63d46a
Chun Chen <chenchun.feed@gmail.com>
2014-12-23 08:15:47 +0800

    Path Sum

--- accumulation-leetcode/src/main/java/org/chenchun/PathSum.java
+++ accumulation-leetcode/src/main/java/org/chenchun/PathSum.java
@@ -17,4 +17,20 @@
  */
 package org.chenchun;
 
-public class PathSum {}
+public class PathSum {
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root != null) {
+      sum -= root.val;
+      if (root.left == null && root.right == null) {
+        return sum == 0;
+      }
+      if (root.left != null && hasPathSum(root.left, sum)) {
+        return true;
+      }
+      if (root.right != null && hasPathSum(root.right, sum)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
