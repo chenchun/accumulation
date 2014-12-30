b4e60081fb8abe287699e5fcc2c04dcdc66efc5a
Chun Chen <chenchun.feed@gmail.com>
2014-12-30 22:32:10 +0800

    Substring with Concatenation of All Words

--- accumulation-leetcode/src/main/java/org/chenchun/SubstringWithConcatenationOfAllWords.java
+++ accumulation-leetcode/src/main/java/org/chenchun/SubstringWithConcatenationOfAllWords.java
@@ -20,40 +20,49 @@ package org.chenchun;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SubstringWithConcatenationOfAllWords {
   public List<Integer> findSubstring(String S, String[] L) {
-    List<Integer> list = new ArrayList<Integer>();
-    if (L == null || S == null || L.length == 0 || L[0].length()*L.length > S.length()) {
-      return list;
-    }
-    HashMap<String, Integer> map = new HashMap<String, Integer>();
-    for (String s : L) {
-      if (!map.containsKey(s)) {
-        map.put(s, 0);
-      }
-      map.put(s, map.get(s)+1);
-    }
-    int l = L[0].length();
-    for (int i = 0; i <= S.length() - L.length*l; i++) {
-      HashMap<String, Integer> m = (HashMap<String, Integer>) map.clone();
-      int j = i;
-      while (m.size()>0) {
-        String s = S.substring(j, j+l);
-        if (m.containsKey(s)) {
-          m.put(s, m.get(s)-1);
-          if (m.get(s) == 0) {
-            m.remove(s);
-          }
-          j+=l;
-        } else {
-          break;
+    List<Integer> list = new ArrayList<>();
+    if (S != null && S.length() != 0 && L != null && L.length != 0) {
+      Map<String, Integer> map = new HashMap<>();
+      for (int i = 0; i < L.length; i++) {
+        String str = L[i];
+        if (!map.containsKey(str)) {
+          map.put(str, 0);
         }
+        map.put(str, map.get(str)+1);
       }
-      if (m.size() == 0) {
-        list.add(i);
+      int l1 = S.length(), l2 = L[0].length(), l3 = l1-l2*L.length;
+      for (int i = 0; i <= l3; i++) {
+        int j = i;
+        Map<String, Integer> subMap = new HashMap<>();
+        while (j+l2 <= l1 && j-i < l2*L.length) {
+          String word = S.substring(j, j+l2);
+          if (!map.containsKey(word)) {
+            break;
+          } else {
+            if (!subMap.containsKey(word)) {
+              subMap.put(word, 0);
+            }
+            subMap.put(word, subMap.get(word)+1);
+            if (map.get(word) < subMap.get(word)) {
+              break;
+            }
+          }
+          j+=l2;
+        }
+        if (map.equals(subMap)) {
+          list.add(i);
+        }
       }
     }
     return list;
+  }
+
+  public static void main(String[] args) {
+    SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+    s.findSubstring("aaa", new String[] {"a", "a"});
   }
 }
