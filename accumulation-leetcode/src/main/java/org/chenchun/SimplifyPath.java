cb24bd651cf7d2089fc32e83dee77c71e0eb925f
Chun Chen <chenchun.feed@gmail.com>
2015-01-03 16:23:14 +0800

    Simplify Path

---
+++ accumulation-leetcode/src/main/java/org/chenchun/SimplifyPath.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.chenchun;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+  public String simplifyPath(String path) {
+    if (path == null || path.length() <= 1) {
+      return path;
+    }
+    int i = 1;
+    Stack<String> stack = new Stack<>();
+    String lastPathName = null;
+    int last = 0;
+    path = path+"/";
+    while (i < path.length()) {
+      char c = path.charAt(i);
+      if (c == '/') {
+        if (last+1 != i) {
+          String s = path.substring(last+1, i);
+          if (".".equals(s)) {
+
+          } else if ("..".equals(s)) {
+            if (!stack.isEmpty()) {
+              stack.pop();
+            }
+          } else {
+            stack.push(s);
+          }
+        }
+        last = i;
+      }
+      i++;
+    }
+    StringBuilder sb = new StringBuilder("");
+    while (!stack.isEmpty()) {
+      sb.insert(0, "/" + stack.pop());
+    }
+    return sb.length() == 0? "/" : sb.toString();
+  }
+}
