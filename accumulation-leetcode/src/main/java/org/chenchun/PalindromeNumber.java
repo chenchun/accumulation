140b190dc06b71a26949a66f585482551ae61885
Chun Chen <chenchun.feed@gmail.com>
2015-01-08 22:41:08 +0800

    Palindrome Number

---
+++ accumulation-leetcode/src/main/java/org/chenchun/PalindromeNumber.java
@@ -0,0 +1,35 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.chenchun;
+
+public class PalindromeNumber {
+  public boolean isPalindrome(int x) {
+    if (x == 0) {
+      return true;
+    }
+    if (x < 0 || x % 10 == 0) {
+      return false;
+    }
+    int half = 0;
+    while (half < x) {
+      half = x%10 + half*10;
+      x/=10;
+    }
+    return half == x || half/10 == x;
+  }
+}
