f54ac16c82627b71b78ddae583ad7d84f770ad41
ramichen <ramichen@tencent.com>
2015-04-15 22:34:51 +0800

    Word Break II

--- accumulation-leetcode/src/main/java/org/chenchun/WordBreakII.java
+++ accumulation-leetcode/src/main/java/org/chenchun/WordBreakII.java
@@ -18,44 +18,42 @@
 package org.chenchun;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 public class WordBreakII {
-  public List<String> wordBreak(String s, Set<String> dict) {
-    List<String> list = new ArrayList<>();
-    if (s == null || s.length() == 0 || dict.size() == 0) {
-      return list;
-    }
-    int l = s.length();
-    List[] dp = new List[l+1];
-    dp[0] = new ArrayList<Integer>();
-    for (int i = 1; i <= l; i++) {
+  public List<String> wordBreak(String s, Set<String> wordDict) {
+    int len = s.length();
+    List<Integer>[] dp = new List[len+1];
+    dp[0] = Collections.emptyList();
+    for (int i = 1; i <= len; i++) {
       for (int j = 0; j < i; j++) {
-        if (dict.contains(s.substring(j, i)) && dp[j] != null) {
+        if (dp[j] != null && wordDict.contains(s.substring(j, i))) {
           if (dp[i] == null) {
-            dp[i] = new ArrayList<Integer>();
+            dp[i] = new ArrayList<>();
           }
           dp[i].add(j);
         }
       }
     }
-    if (dp[l] == null) {
+    List<String> list = new ArrayList<>();
+    if (dp[len] == null) {
       return list;
     }
-    word(dp, l, "", s, list);
+    dfs(dp, list, s, null, len);
     return list;
   }
 
-  private void word(List[] dp, int end, String part, String s, List<String> list) {
-    if (end != 0) {
-      List<Integer> indexs = (List<Integer>) dp[end];
-      for (int i = 0; i < dp[end].size(); i++) {
-        int b = indexs.get(i);
-        word(dp, b, s.substring(b, end) + (part.isEmpty()? part : " "+part), s, list);
-      }
-    } else {
-      list.add(part);
+  private void dfs(List<Integer>[] dp, List<String> ret, String s, String part, int index) {
+    if (index == 0) {
+      ret.add(part);
+      return;
+    }
+    part = part == null? "" : " " + part;
+    for (int i = 0; i < dp[index].size(); i++) {
+      int start = dp[index].get(i);
+      dfs(dp, ret, s, s.substring(start, index) + part, start);
     }
   }
 }
