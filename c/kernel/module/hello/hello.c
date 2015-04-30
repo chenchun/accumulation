50391d254be5942a8df0579526f320be4d0026cf
Chun Chen <chenchun.feed@gmail.com>
2015-04-30 15:06:26 +0800

    use strace to trace system call example

---
+++ c/kernel/module/hello/hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年04月30日 星期四 10时39分08秒
+    > http://www.tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN441
+    > gcc -Wall -o hello hello.c
+    > strace ./hello
+ ************************************************************************/
+
+#include <stdio.h>
+int main(void)
+{ printf("hello"); return 0;  }
--- c/kernel/module/hello/readme.md
+++ c/kernel/module/hello/readme.md
@@ -1,4 +1,8 @@
 ## install and uninstall kernel module
 
-insmod ./hello-1.ko
-rmmod hello-1
+    insmod ./hello-1.ko
+    rmmod hello-1
+
+## module info
+
+    modinfo startstop.ko
