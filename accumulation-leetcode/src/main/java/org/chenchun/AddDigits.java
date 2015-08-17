e4b7a1a6fe571c8a8f8e4ff6937aaf9252c9ddbc
Chun Chen <ramichen@tencent.com>
2015-08-17 22:28:47 +0800

    Add Digits

---
+++ accumulation-leetcode/src/main/java/org/chenchun/AddDigits.java
@@ -0,0 +1,15 @@
+package org.chenchun;
+
+public class AddDigits {
+  public int addDigits(int num) {
+    if (num < 10) {
+      return num;
+    }
+    int sum = 0;
+    while (num != 0) {
+      sum += num%10;
+      num /= 10;
+    }
+    return addDigits(sum);
+  }
+}
