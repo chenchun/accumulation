65c85f0f741678a615ddb1f2ccc74e752fbd58c9
Chun Chen <ramichen@tencent.com>
2016-04-25 23:41:55 +0800

    Rewrite Kth Largest Element in an Array

--- accumulation-leetcode/src/main/java/org/chenchun/KthLargestElementinanArray.java
+++ accumulation-leetcode/src/main/java/org/chenchun/KthLargestElementinanArray.java
@@ -19,45 +19,28 @@ package org.chenchun;
 
 public class KthLargestElementinanArray {
   public int findKthLargest(int[] nums, int k) {
-    int left = 0, right = nums.length-1;
-    while (true) {
-      int i = left, j = right;
-      int pivot = nums[i++];
-      while (i <= j) {
-        while (nums[i] >= pivot && i < right) {
-          i++;
-        }
-        while (nums[j] < pivot) {
-          j--;
-        }
-        if (i <= j) {
-          int t = nums[i];
-          nums[i] = nums[j];
-          nums[j] = t;
-          i++;
-          j--;
-        }
-      }
-      if (i-left == k) {
-        return pivot;
-      } else if (i-left < k) {
-        k = k - i + left;
-        left = i;
-      } else {
-        right = i-1;
-        left = left+1;
-      }
-    }
+    return findKthLargest(nums, nums.length+1-k, 0, nums.length-1);
   }
 
-  public static void main(String[] args) {
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1, 4, 3, 5}, 1));
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1, 4, 3, 5}, 2));
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1, 4, 3, 5}, 3));
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1, 4, 3, 5}, 4));
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1, 4, 3, 5}, 5));
-//    System.out.println(new KthLargestElementinanArray().findKthLargest(new int[]{2, 1}, 2));
-    System.out.println(new KthLargestElementinanArray().findKthLargest(new
-        int[]{1,1}, 2));
+  public int findKthLargest(int[] nums, int k, int start, int end) {
+    if (start == end) {
+      return nums[start];
+    }
+    int l = start, r = end;
+    int p = (nums[l]+nums[r])/2;
+    while (l < r) {
+      while (l < r && nums[l] <= p) l++;
+      while (nums[r] > p) r--;
+      if (l < r) {
+        int t = nums[l];
+        nums[l] = nums[r];
+        nums[r] = t;
+      }
+    }
+    if (l-start >= k) {
+      return findKthLargest(nums, k, start, l-1);
+    } else {
+      return findKthLargest(nums, k-l+start, l, end);
+    }
   }
 }
